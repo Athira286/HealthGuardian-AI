1bbaddd9cee4214e2ffd5db3c44703654fa5cf3e
Priya <athiraanish13@gmail.com>
2026-07-16 10:44:18 +0530

    Changed firebase_config

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,8 +1,23 @@
+import os
+import json
+import base64
 import firebase_admin
+
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
+
+    encoded_key = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+
+    if encoded_key:
+        decoded = base64.b64decode(encoded_key)
+        service_account = json.loads(decoded)
+
+        cred = credentials.Certificate(service_account)
+
+    else:
+        cred = credentials.Certificate("serviceAccountKey.json")
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
